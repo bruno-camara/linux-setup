d4bfdd6813c2c32e638aa9afe707f05a68ecf6f7
bruno-camara <brunao.camara@gmail.com>
2024-11-12 15:06:02 -0300

    :art: improve git config file

--- config/config_git.sh
+++ config/config_git.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # script to configure my Git
 
-GITHUB_USER="bruno-camara"
-GITHUB_EMAIL="brunao.camara@gmail.com"
+source ../infos.sh
 
 git config --global user.name "$GITHUB_USER"
-git config --global user.email "$GITHUB_EMAIL"
+git config --global user.email "$EMAIL"
